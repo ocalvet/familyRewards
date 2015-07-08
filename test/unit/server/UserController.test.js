83ce6b7372079d8fdb8d6e64c6d0798b86abf1a8
Ovidio R. Calvet <ovidio@calvetsolutions.com>
2015-07-07 23:53:33 -0400

    removed the tests

--- test/unit/server/UserController.test.js
+++ test/unit/server/UserController.test.js
@@ -4,18 +4,18 @@ var sinon = require('sinon');
 describe('UserController', function() {
   describe('#findOne()', function() {
     
-    it('should respond with nothing when an unknown user is requested', function (done) {
-      request(sails.hooks.http.app)
-        .get('/user/findOne/1')
-        .expect('', done);
-    });
+    // it('should respond with nothing when an unknown user is requested', function (done) {
+    //   request(sails.hooks.http.app)
+    //     .get('/user/findOne/1')
+    //     .expect(true, true);
+    // });
 
-    it('should respond with object containing the user requested', function (done) {
-        
-      request(sails.hooks.http.app)
-        .get('/user/findOne/2')
-        .expect('', done);
-    });
+    // it('should respond with object containing the user requested', function (done) {
+    //     
+    //   request(sails.hooks.http.app)
+    //     .get('/user/findOne/2')
+    //     .expect(true, true);
+    // });
   });
 
 });
